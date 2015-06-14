fbe884ea3edc3dad160551e141e99794df2c4420
Vincent San Miguel <vincent.sanmiguel1@gmail.com>
2015-06-14 16:45:45 -0400

    Updated median function

--- src/statistics.f90
+++ src/statistics.f90
@@ -4,133 +4,151 @@ module statistics
   implicit none
   
   public :: factorial, mean, variance, standardDeviation, covariance
+
+  public
+
+  interface median
+     module procedure median_real, median_int
+  end interface median
   
 contains
 
-pure integer function factorial(x) result(xfac)
-  !! Returns the factorial of the scalar value \(\mathbf{x}\),
-  !! $$ x! = n(n-1)(n-2)\cdots(3)(2)(1) $$
+  pure integer function factorial(x) result(xfac)
+    !! Returns the factorial of the scalar value \(\mathbf{x}\),
+    !! $$ x! = n(n-1)(n-2)\cdots(3)(2)(1) $$
 
-  integer, intent(in) :: x
+    integer, intent(in) :: x
 
-  integer :: i
+    integer :: i
+
+    xfac = 1
+    if (x >= 0) then
+       do i = x, 1, -1
+          xfac = xfac * i
+       end do
+    end if
+
+  end function factorial
+
+  pure real function mean(x) result(mu)
+    !! Returns the arithmatic mean of a vector \(\mathbf{x}\).
+    !! $$ \mu = \frac{1}{n} \sum_{i=1}^n x_i $$
+
+    implicit none
+
+    real, intent(in) :: x(:)
+
+    real :: Sx
+    integer :: n, i, l, u
+
+    mu = 0.0
+    Sx = 0.0
+
+    n = size(x)
+    l = lbound(x,1)
+    u = ubound(x,1)
+
+    do i = l, u
+       Sx = Sx + x(i)
+    end do
+
+    mu = Sx / n
+
+  end function mean
   
-  xfac = 1
-  if (x >= 0) then
-     do i = x, 1, -1
-        xfac = xfac * i
-     end do
-  end if
+  pure real function median_int(x) result(xm)
 
-end function factorial
-  
-pure real function mean(x) result(mu)
-  !! Returns the arithmatic mean of a vector \(\mathbf{x}\).
-  !! $$ \mu = \frac{1}{n} \sum_{i=1}^n x_i $$
+    integer, intent(in) :: x(:)
 
-  implicit none
+    integer :: n, nd2
+    real :: xmm1, xmp1
 
-  real, allocatable, intent(in) :: x(:)
-  
-  real :: Sx
-  integer :: n, i, l, u
-
-  mu = 0.0
-  Sx = 0.0
-  
-  if (allocated(x)) then
      n = size(x)
-     l = lbound(x,1)
-     u = ubound(x,1)
-     
-     do i = l, u
-        Sx = Sx + x(i)
-     end do
+    nd2 = ceiling(n/2.0)
 
-     mu = Sx / n
-  end if
-end function mean
-
-pure real function median(x) result (xm)
-  !! Returns the median of a vector. If the vector has an odd number
-  !! of elements, the "mean of middle" two is returned.
-
-  implicit none
-
-  real, allocatable, intent(in) :: x(:)
-
-  integer :: n, nd2
-  real :: xmm1, xmp1
-
-  n = size(x)
-  nd2 = n/2
-
-  if (mod(n,2) .eq. 0) then
-     xm = x(nd2)
-  else
-     xmm1 = x(nd2-1)
-     xmp1 = x(nd2+1)
-     xm = (xmm1 + xmp1) / 2.0
-  end if
+    if (mod(n,2) .eq. 0) then
+       xm = x(nd2)
+    else
+       xmm1 = x(nd2-1)
+       xmp1 = x(nd2)
+       xm = (xmm1 + xmp1) / 2.0
+    end if
+  end function median_int
   
-end function median
+  pure real function median_real(x) result (xm)
 
-pure real function variance(x) result(ss)
-  !! Returns the sample variance of the vector \(\mathbf{x}\),
-  !! $$ s^2 = \frac{1}{n-1} \sum_{i=1}^n \left(x_i - \mu_x \right)^2 $$
+    real, intent(in) :: x(:)
+    
+    integer :: n, nd2
+    real :: xmm1, xmp1
+    
+    n = size(x)
+    nd2 = ceiling(n/2.0)
 
-  implicit none
-
-  real, allocatable, intent(in) :: x(:)
-
-  integer :: n, i, l, u
-  real :: xb
-
-  n = size(x)
-
-  xb = mean(x)
-  ss = 0.0
+    if (mod(n,2) .eq. 0) then
+       xm = x(nd2)
+    else
+       xmm1 = x(nd2-1)
+       xmp1 = x(nd2)
+       xm = (xmm1 + xmp1) / 2.0
+    end if
   
-  if (allocated(x)) then
-     l = lbound(x,1)
-     u = ubound(x,1)
-     do i = l, u
-        ss = ss + (x(i) - xb)**2
-     end do
+  end function median_real
 
-     ss = 1 / (n - 1) * ss
-  end if
-  
-end function variance
+  pure real function variance(x) result(ss)
+    !! Returns the sample variance of the vector \(\mathbf{x}\),
+    !! $$ s^2 = \frac{1}{n-1} \sum_{i=1}^n \left(x_i - \mu_x \right)^2 $$
 
-pure real function standardDeviation(x) result(s)
-  !! Returns the sample standard deviation of a vector \(\mathbf{x}\),
-  !! $$ s = \sqrt{\frac{1}{n-1} \sum_{i=1}^n \left(x_i - \mu_x \right)^2} $$
+    implicit none
 
-  implicit none
+    real, intent(in) :: x(:)
 
-  real, allocatable, intent(in) :: x(:)
+    integer :: n, i, l, u
+    real :: xb
 
-  s = sqrt(variance(x))
+    n = size(x)
 
-end function standardDeviation
+    xb = mean(x)
+    ss = 0.0
 
-pure real function covariance(x, y) result(sigma)
-  !! Returns the covariance of two vectors \(\mathbf{x}\) and \(\mathbf{y}\),
-  !! $$ \sigma(x,y) =\frac{1}{n-1} \sum_{i=1}^n \left(x_i - \mu_x \right) \left(y_i-\mu_y\right) $$
+    l = lbound(x,1)
+    u = ubound(x,1)
+    do i = l, u
+       ss = ss + (x(i) - xb)**2
+    end do
 
-  implicit none
-  
-  real, allocatable, intent(in) :: x(:), y(:)
-  
-  real :: Sx, Sy, xb, yb
-  integer :: n, i, l, u
+    ss = 1 / (n - 1) * ss
 
-  !!@todo If x and y aren't same size, return error
-  !!@todo Handle x and y not same bound start/end
-  sigma = 0.0
-  
-  if (allocated(x).and. allocated(y)) then
+  end function variance
+
+  pure real function standardDeviation(x) result(s)
+    !! Returns the sample standard deviation of a vector \(\mathbf{x}\),
+    !! $$ s = \sqrt{\frac{1}{n-1} \sum_{i=1}^n \left(x_i - \mu_x \right)^2} $$
+
+    implicit none
+
+    real, intent(in) :: x(:)
+
+    s = sqrt(variance(x))
+
+  end function standardDeviation
+
+  pure real function covariance(x, y) result(sigma)
+    !! Returns the covariance of two vectors \(\mathbf{x}\) and \(\mathbf{y}\),
+    !! $$ \sigma(x,y) =\frac{1}{n-1} \sum_{i=1}^n \left(x_i - \mu_x \right) \left(y_i-\mu_y\right) $$
+
+    implicit none
+
+    real, intent(in) :: x(:), y(:)
+
+    real :: Sx, Sy, xb, yb
+    integer :: n, i, l, u
+
+    !!@todo If x and y aren't same size, return error
+    !!@todo Handle x and y not same bound start/end
+    sigma = 0.0
+
+    
     n = size(x)
 
     xb = mean(x)
@@ -139,13 +157,12 @@ pure real function covariance(x, y) result(sigma)
     l = lbound(x,1)
     u = ubound(x,1)
     do i = l, u
-      Sx = x(i) - xb
-      Sy = y(i) - yb
-      sigma = sigma + (Sx*Sy / n)
+       Sx = x(i) - xb
+       Sy = y(i) - yb
+       sigma = sigma + (Sx*Sy / n)
     end do
-  end if
 
-end function covariance
+  end function covariance
 
 end module statistics
 
